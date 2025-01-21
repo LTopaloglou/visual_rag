8b29898527bdc661c38370b529526843cbebab7e
Leo <leo.topalogloumundy@gmail.com>
2025-01-21 14:23:38 -0500

    typo fix

--- vqa.py
+++ vqa.py
@@ -18,16 +18,15 @@ class VisualQA:
 
     def predict(self, image_paths: List[str], queries: List[str]) -> str:
         image_messages = [{"type": "image", "image": path} for path in image_paths]
-        query_messages = [{"type:": "text", "text": query} for query in queries]
+        query_messages = [{"type": "text", "text": query} for query in queries]
         messages = [{
             "role": "user",
             "content": image_messages + query_messages,
         }]
         text = self.processor.apply_chat_template(
-            messages[0], tokenize=False, add_generation_prompt=True
+            messages, tokenize=False, add_generation_prompt=True
         )
-        print(messages)
-        image_inputs, video_inputs = process_vision_info(messages) #This should be list of dict or list of list of dict
+        image_inputs, video_inputs = process_vision_info(messages)
         inputs = self.processor(
             text=[text],
             images=image_inputs,
